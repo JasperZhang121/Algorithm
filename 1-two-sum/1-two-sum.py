ec4800b8b6bc358b2cb14b5b9f7c666eb39c87ef
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2022-10-10 11:13:20 +1100

    Time: 2153 ms (26.37%), Space: 15.2 MB (25.14%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        mapp = {}
+        i=0
+        for x in nums:
+            mapp[i] = x
+            if target-x in mapp.values():
+                for z,w in mapp.items():
+                    if w == target-x and i!=z:
+                        return [z,i]
+            i+=1     
