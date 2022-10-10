26518ade8a204f72cdd491a0c06d39a35cd7e056
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2022-10-10 19:13:53 +1100

    Time: 17 ms (38.68%), Space: 44.1 MB (78.60%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0||(x%10==0 && x!=0))return false;
+    
+        int r =0;
+        while(x>r){
+            r=r*10+x%10;
+            x/=10;
+        }
+        return r==x||r/10==x;
+    }      
+}
