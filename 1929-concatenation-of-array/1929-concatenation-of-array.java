e6e9edcd15e0ae64eab9c5399f8a7f77c3136923
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2022-11-26 04:31:25 +1100

    Time: 6 ms (15.37%), Space: 49.7 MB (74.36%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,3 +1,9 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums+nums
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        var res = new int[nums.length*2];
+        for (int i = 0; i < nums.length*2; i++) {
+            res[i] = nums[i% nums.length];
+        }
+        return res;     
+    }
+}
