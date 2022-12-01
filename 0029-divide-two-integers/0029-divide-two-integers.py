f3fc08eaddb18356ee3bdf9865d43df896635d90
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2022-12-01 17:37:44 +1100

    Time: 64 ms (47.92%), Space: 14 MB (27.06%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -20,32 +20,33 @@ class Solution:
             negatives -= 1
             divisor = -divisor
 
-        # In the first loop, we simply find the largest double of divisor
-        # that fits into the dividend.
-        # The >= is because we're working in negatives. In essence, that
-        # piece of code is checking that we're still nearer to 0 than we
-        # are to INT_MIN.
-        highest_double = divisor
-        highest_power_of_two = -1
-        while highest_double >= HALF_MIN_INT and dividend <= highest_double + highest_double:
-            highest_power_of_two += highest_power_of_two
-            highest_double += highest_double
+        doubles = []
+        powersOfTwo = []
 
-        # In the second loop, we work out which powers of two fit in, by
-        # halving highest_double and highest_power_of_two repeatedly.
-        # We can do this using bit shifting so that we don't break the
-        # rules of the question :-)
+        # Nothing too exciting here, we're just making a list of doubles of 1 and
+        # the divisor. This is pretty much the same as Approach 2, except we're
+        # actually storing the values this time. */
+        powerOfTwo = 1
+        while divisor >= dividend:
+            doubles.append(divisor)
+            powersOfTwo.append(powerOfTwo)
+            # Prevent needless overflows from occurring...
+            if divisor < HALF_MIN_INT:
+                break
+            divisor += divisor # Double divisor
+            powerOfTwo += powerOfTwo
+
+        # Go from largest double to smallest, checking if the current double fits.
+        # into the remainder of the dividend.
         quotient = 0
-        while dividend <= divisor:
-            if dividend <= highest_double:
-                quotient += highest_power_of_two
-                dividend -= highest_double
-            # We know that these are always even, so no need to worry about the
-            # annoying "bit-shift-odd-negative-number" case.
-            highest_power_of_two >>= 1
-            highest_double >>= 1
+        for i in reversed(range(len(doubles))):
+            if doubles[i] >= dividend:
+                # If it does fit, add the current powerOfTwo to the quotient.
+                quotient += powersOfTwo[i]
+                # Update dividend to take into account the bit we've now removed.
+                dividend -= doubles[i]
 
         # If there was originally one negative sign, then
         # the quotient remains negative. Otherwise, switch
         # it to positive.
-        return quotient if negatives == 1 else -quotient
+        return quotient if negatives != 1 else -quotient
