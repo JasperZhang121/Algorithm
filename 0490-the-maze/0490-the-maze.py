02711c3859f0fd3b505353ff715bb5922d391805
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2023-01-23 23:14:18 +1100

    Time: 1044 ms (9.47%), Space: 14.4 MB (89.43%) - LeetHub

---
+++ 0490-the-maze/0490-the-maze.py
@@ -0,0 +1,28 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        
+        Q = [start]
+        n = len(maze)
+        m = len(maze[0])
+        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        
+        while Q:
+            i, j = Q.pop(0)
+            maze[i][j] = 2
+
+            if i == destination[0] and j == destination[1]:
+                return True
+            
+            for x, y in dirs:
+                row = i + x
+                col = j + y
+                while 0 <= row < n and 0 <= col < m and maze[row][col] != 1:
+                    row += x
+                    col += y
+                row -= x
+                col -= y
+                if maze[row][col] == 0:
+                    Q.append([row, col])
+        
+        return False
+    
