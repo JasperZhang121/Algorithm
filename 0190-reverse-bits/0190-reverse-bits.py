057eafab95e2a78b7d3a01fd789510dd2b7653f5
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2023-01-29 13:35:57 +1100

    Time: 34 ms (77.06%), Space: 13.8 MB (92.96%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 31
+        while n:
+            ret += (n & 1) << power
+            n = n >> 1
+            power -= 1
+        return ret        
