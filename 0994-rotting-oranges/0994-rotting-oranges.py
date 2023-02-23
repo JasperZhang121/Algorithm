820796d462c57efd1dc77884119d9ec32c4e2bb7
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2023-02-23 21:20:07 +1100

    Time: 58 ms (45.50%), Space: 13.9 MB (86.79%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        visit, curr = set(), deque()
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    visit.add((i, j))
+                elif grid[i][j] == 2:
+                    curr.append((i, j))
+        result = 0
+        while visit and curr:
+			# BFS iteration
+            for _ in range(len(curr)):
+                i, j = curr.popleft()  # obtain recent rotten orange
+                for coord in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+                    if coord in visit:  # check if adjacent orange is fresh
+                        visit.remove(coord)
+                        curr.append(coord)
+            result += 1
+            
+        return -1 if visit else result
+            
