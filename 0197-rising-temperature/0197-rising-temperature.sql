2a4decaae57ce960e1fdad5a78ce1e34876c38d1
Hao Zhang <111576857+JasperZhang121@users.noreply.github.com>
2023-03-04 16:08:27 +1100

    Time: 664 ms (90.64%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,6 @@
 
 select w1.id from weather w1
 
-        join weather w2 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
-            where w1.temperature > w2.temperature
+    join weather w2 on w1.recordDate = date_add(w2.recordDate, interval 1 day)
+        where w1.temperature > w2.temperature
+    
