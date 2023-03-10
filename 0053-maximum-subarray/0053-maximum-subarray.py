4bb55f9ec0c6e4ca3d8fd2650a5eee9a90047068
JasperZhang121 <111576857+JasperZhang121@users.noreply.github.com>
2023-03-11 03:15:37 +1100

    Time: 821 ms (26.39%), Space: 28.6 MB (83.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,6 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
+        # use dp for solving 
+        
+        # each pos in dp array is the max number we can get from previous subarry
+        
+        # if the current number larger than previous sub array
+        
+        # we record the current number meaning start a new array
+        
         n = len(nums)
         dp = [0] * n
         dp[0] = nums[0]
@@ -9,5 +17,3 @@ class Solution:
             dp[i] = max(dp[i-1] + nums[i], nums[i])
             max_sum = max(dp[i], max_sum)
         return max_sum
-        
-        
