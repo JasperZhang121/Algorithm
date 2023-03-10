3d544e49f03fa023763c9a9bfcd382fcb1c492bd
JasperZhang121 <111576857+JasperZhang121@users.noreply.github.com>
2023-03-11 03:21:36 +1100

    Time: 835 ms (20.59%), Space: 28.6 MB (83.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,11 +3,14 @@ class Solution:
         
         # use dp for solving 
         
-        # each pos in dp array is the max number we can get from previous subarry
+        # each pos in dp array is the max number at that position we can get from any possible combination of previous subarrys
+        # even it decreases, but it is the max in that position
         
-        # if the current number larger than previous sub array
-        
+        # if the current number > previous sub array + current number (meaning previous is negative) 
         # we record the current number meaning start a new array
+        
+        # else the current number < previous sub array + current number (the max number we can get so far at this positon even it decreases)
+        # record the sum
         
         n = len(nums)
         dp = [0] * n
