e68ee9a2cb765be0bb2a90ed1ee3980df9df144a
JasperZhang121 <111576857+JasperZhang121@users.noreply.github.com>
2023-03-11 03:25:44 +1100

    Time: 815 ms (29.05%), Space: 28.5 MB (83.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,17 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        # use dp for solving 
-        
-        # each pos in dp array is the max number at that position we can get from any possible combination of previous subarrys
-        # even it decreases, but it is the max in that position
-        
-        # if the current number > previous sub array + current number (meaning previous is negative) 
-        # we record the current number meaning start a new array
-        
-        # else the current number < previous sub array + current number (the max number we can get so far at this positon even it decreases)
-        # record the sum
-        
         n = len(nums)
         dp = [0] * n
         dp[0] = nums[0]
