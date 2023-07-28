b689696f92e8217db4ffefcf7e78285c31aa2f21
JasperZhang121 <111576857+JasperZhang121@users.noreply.github.com>
2023-07-28 14:06:01 +1000

    Time: 34 ms (98.74%), Space: 16.3 MB (47.17%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,10 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1,len2 = len(str1),len(str2)
         
-        if len(str1) < len(str2):
-            str1, str2 = str2, str1
-        if str1 == str2:
-            return str1
-        if str1[:len(str2)] != str2:
-            return ""
-        return self.gcdOfStrings(str1[len(str2):], str2)      
+        def isDivisible(l):
+            if len1%l or len2%l:
+                return False
+            f1,f2 = len1//l,len2//l
+            return str1[:l]*f1==str1 and str2[:l]*f2==str2 and str1[:l] == str2[:l]
+        
+        for l in range(min(len1,len2),0,-1):
+            if isDivisible(l):
+                return str1[:l]
+        return ""
