39649589d10ff3d9fb8867a4107f3fb6dc678246
JasperZhang121 <111576857+JasperZhang121@users.noreply.github.com>
2024-04-03 19:19:05 +0800

    Time: 208 ms (25.72%), Space: 45.3 MB (5.55%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, int row, int col, String word, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[row][col];
+        board[row][col] = '#'; // mark as visited
+
+        int[] rowOffsets = {0, 1, 0, -1};
+        int[] colOffsets = {1, 0, -1, 0};
+        for (int d = 0; d < 4; d++) {
+            if (dfs(board, row + rowOffsets[d], col + colOffsets[d], word, index + 1)) {
+                return true;
+            }
+        }
+
+        board[row][col] = temp; // unmark
+        return false;
+    }
+}
